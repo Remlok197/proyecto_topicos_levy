dbf837dcafecd41705512369b13897f0b5d227f9
Remlok197 <emiliod0204@gmail.com>
2026-05-27 22:17:35 -0600

    Creacion de rutas y API funcionando correctamente

--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,14 @@ package main
 import (
 	"fmt"
 	"proyecto-topicos-backend/config"
-
+	"proyecto-topicos-backend/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.ConnectDatabase()
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"mensaje": "Server Gin y conexión a MySQL funcionando mi gente",
-		})
-	})
+	routes.SetupRoutes(r)
 	fmt.Println("Arrancando server en http://localhost:8080")
 	r.Run(":8080")
 }
