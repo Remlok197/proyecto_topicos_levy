bad5481d4b58ccc14176ca1235668337f13a2132
Fabián Torres <23241003@leon.tecnm.mx>
2026-05-29 19:35:57 -0600

    Añadir tabla de empleados

--- client/electron-app/src/renderer/src/pages/crear/CrearPage.jsx
+++ client/electron-app/src/renderer/src/pages/crear/CrearPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import HeaderPager from './HeaderPager'
+import HeaderPager from '../../components/shared/HeaderPager'
 import Form from './Form'
 
 export default function CrearPage() {
   return (
     <main className="flex-1 overflow-y-auto bg-slate-50/50 p-6 md:p-8 flex flex-col items-center">
       <div className="w-full max-w-4xl">
-        <HeaderPager />
+        <HeaderPager
+          title="Registro de Nuevo Empleado"
+          subtitle="Ingrese los detalles de la nueva contratación para comenzar el proceso de incorporación."
+        />
         <Form />
       </div>
     </main>
