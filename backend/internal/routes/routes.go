8f63441da4a9dc2c507b8a7e9ab1812dc32b79f4
Fabián Torres <23241003@leon.tecnm.mx>
2026-05-30 19:59:07 -0600

    Añadir documentación simple de la API

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,13 +1,30 @@
 package routes
 
 import (
+	_ "employees-api/docs"
 	"employees-api/internal/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // SetupRoutes configura todos los endpoints de nuestra API
 func SetupRoutes(router *gin.Engine, empHandler *handlers.EmployeeHandler) {
+	// Redirigir la raíz (/) a la documentación
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/docs/index.html")
+	})
+
+	// Redirigir /docs
+	router.GET("/docs", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/docs/index.html")
+	})
+
+	// Endpoint de Swagger
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	api := router.Group("/api")
 	{
 		api.GET("/employees", empHandler.GetEmployees)
